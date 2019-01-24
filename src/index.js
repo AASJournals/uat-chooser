a78e03d519f63b570a22d7786687a598822d7472
Peter Williams <peter@newton.cx>
2019-01-24 18:34:33 -0500

    src/index.js: add a header comment here

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+// Copyright 2019 the American Astronomical Society
+// Licensed under the MIT License.
+//
+// Due to the current way in which we build our webpack module, this file is a
+// small shim that comes at the end of a concatenation several JavaScript
+// files -- see `../package.json`. In particular, this file comes right after
+// `./ejp_uat.js` which currently has all of the application-specific code.
+
 import '../src/ejp_uat.css';
 
 import img_indicator_tiny_red from '../img/indicator_tiny_red.gif';
